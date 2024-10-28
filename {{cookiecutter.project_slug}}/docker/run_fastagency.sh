a2ace6a41892ed6f50fbfdf3bd90e9cbc53318d5
Kumaran Rajendhiran <kumaran@airt.ai>
2024-10-29 00:00:05 +0530

    WIP: Add dockerfile for all app types

--- {{cookiecutter.project_slug}}/docker/run_fastagency.sh
+++ {{cookiecutter.project_slug}}/docker/run_fastagency.sh
@@ -17,7 +17,8 @@ WORKERS=${WORKERS:-1}
 uvicorn {{cookiecutter.project_slug}}.main_1_nats:app --host 0.0.0.0 --port $NATS_FASTAPI_PORT > /dev/stdout 2>&1 &
 {% endif %}
 # Run uvicorn server
-uvicorn {{cookiecutter.project_slug}}.main_2_fastapi:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
-
+uvicorn {{cookiecutter.project_slug}}.main_{% if "nats" in cookiecutter.app_type %}2_fastapi{% elif "fastapi" in cookiecutter.app_type %}_1_fastapi{% endif %}:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
+{% if "mesop" in cookiecutter.app_type %}
 # Run gunicorn server
-gunicorn --workers=$WORKERS {{cookiecutter.project_slug}}.main_3_mesop:app --bind 0.0.0.0:$MESOP_PORT
+gunicorn --workers=$WORKERS {{cookiecutter.project_slug}}.main{% if "nats" in cookiecutter.app_type %}_3_mesop{% elif "fastapi" in cookiecutter.app_type %}_2_mesop{% endif %}:app --bind 0.0.0.0:$MESOP_PORT
+{% endif %}
