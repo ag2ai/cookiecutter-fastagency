fbf882c729cdb594a516a4ba7e3ffd0aa559c560
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-07 12:48:17 +0530

    Sticky sessions using nginx (#22)
    * Add support for session affinity with nginx ip_hash and hardcoded workers and port
    
    * Make mesop port and workers configurable
    
    * Remove commented out line
    
    * Use non root user and grant privileges to start nginx

--- {{cookiecutter.project_slug}}/docker/run_fastagency.sh
+++ {{cookiecutter.project_slug}}/docker/run_fastagency.sh
@@ -7,11 +7,23 @@ NATS_FASTAPI_PORT=${NATS_FASTAPI_PORT:-8000}
 {% if "fastapi" in cookiecutter.app_type %}
 FASTAPI_PORT=${FASTAPI_PORT:-8008}
 {% endif %}
-{% if "mesop" in cookiecutter.app_type %}
-MESOP_PORT=${MESOP_PORT:-8888}
-{% endif %}
+export MESOP_PORT=${MESOP_PORT:-8888}
+
 # Default number of workers if not set
 WORKERS=${WORKERS:-1}
+echo "Number of workers: $WORKERS"
+
+# Generate nginx config
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((MESOP_PORT + i))
+    sed -i "5i\    server 127.0.0.1:$PORT;" nginx.conf.template
+done
+envsubst '${MESOP_PORT}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
+
+# Start nginx
+nginx -g "daemon off;" &
 {% if "nats" in cookiecutter.app_type %}
 # Run nats uvicorn server
 uvicorn {{cookiecutter.project_slug}}.deployment.main_1_nats:app --host 0.0.0.0 --port $NATS_FASTAPI_PORT > /dev/stdout 2>&1 &
@@ -20,5 +32,13 @@ uvicorn {{cookiecutter.project_slug}}.deployment.main_1_nats:app --host 0.0.0.0 
 uvicorn {{cookiecutter.project_slug}}.deployment.main_{% if "nats" in cookiecutter.app_type %}2_fastapi{% elif "fastapi" in cookiecutter.app_type %}1_fastapi{% endif %}:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
 {% if "mesop" in cookiecutter.app_type %}
 # Run gunicorn server
-gunicorn --workers=$WORKERS {{cookiecutter.project_slug}}.deployment.main{% if "nats" in cookiecutter.app_type %}_3_mesop{% elif "fastapi" in cookiecutter.app_type %}_2_mesop{% endif %}:app --bind 0.0.0.0:$MESOP_PORT
+# Start multiple single-worker gunicorn instances on consecutive ports
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((MESOP_PORT + i))
+    echo "Starting gunicorn on port $PORT"
+    gunicorn --workers=1 {{cookiecutter.project_slug}}.deployment.main{% if "nats" in cookiecutter.app_type %}_3_mesop{% elif "fastapi" in cookiecutter.app_type %}_2_mesop{% endif %}:app --bind 0.0.0.0:$PORT > /dev/stdout 2>&1 &
+done
 {% endif %}
+# Wait for all background processes
+wait
