7e9fea28b069fcb030a6ff0dfa9d1113f67409e1
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-08 14:54:58 +0530

    Change echo to print in green colour

--- scripts/test_cookiecutter.sh
+++ scripts/test_cookiecutter.sh
@@ -6,17 +6,17 @@
 # The cookiecutter replay file is used to provide the default values for the cookiecutter template
 
 # Accept two arguments: app-type and python-version
-echo "Generating project using cookiecutter template with app-type: $1 and python-version: $2"
+echo -e "\033[32mGenerating project using cookiecutter template with app-type: $1 and python-version: $2\033[0m"
 cookiecutter -f --replay-file ./cookiecutter_replay/$1_$2.json --output-dir generated/ ./ 
 
 # Install generated project's dependencies
-echo "Installing dependencies for the generated project"
+echo -e "\033[32mInstalling dependencies for the generated project\033[0m"
 cd generated/my_fastagency_app && pip install -e .[dev] && cd ../../
 
 # Initialize git in the generated project(needed for pre-commit)
-echo "Initializing git in the generated project"
+echo -e "\033[32mInitializing git in the generated project\033[0m"
 cd generated/my_fastagency_app && git init && git add . && cd ../../
 
 # Run pre-commit
-echo "Running pre-commit"
+echo -e "\033[32mRunning pre-commit\033[0m"
 cd generated/my_fastagency_app && pre-commit run --show-diff-on-failure --color=always --all-files && cd ../../
