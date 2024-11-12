1b5284f839d8162cb2c8d34a6b0947d24e6f3a7a
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-12 19:14:55 +0530

    Add basic auth as new authentication option

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/deployment/main_{% if "nats" in cookiecutter.app_type %}3{% else %}2{% endif %}_mesop.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/deployment/main_{% if "nats" in cookiecutter.app_type %}3{% else %}2{% endif %}_mesop.py
@@ -6,7 +6,8 @@ import yaml
 from fastagency.adapters.fastapi import FastAPIAdapter
 from fastagency.app import FastAgency
 from fastagency.ui.mesop import MesopUI{% if cookiecutter.authentication == "google" %}
-from fastagency.ui.mesop.auth.firebase import FirebaseAuth, FirebaseConfig{% endif %}
+from fastagency.ui.mesop.auth.firebase import FirebaseAuth, FirebaseConfig{% elif cookiecutter.authentication == "basic" %}
+from fastagency.ui.mesop.auth.basic_auth import BasicAuth{% endif %}
 
 fastapi_url = "http://localhost:8008"
 
@@ -24,6 +25,19 @@ auth = FirebaseAuth(
     sign_in_methods=["google"],
     config=firebase_config,
     allowed_users=allowed_users,
+)
+
+ui = MesopUI(auth=auth)
+{%- elif cookiecutter.authentication == "basic" %}
+auth = BasicAuth(
+    # TODO: Replace `allowed_users` with the desired usernames and their
+    # bcrypt-hashed passwords. One way to generate bcrypt-hashed passwords
+    # is by using online tools such as https://bcrypt.online
+    # Default password for all users is `password`
+    allowed_users={
+        "admin": "$2y$10$ZgcGQlsvMoMRmmW4Y.nUVuVHc.vOJsOA7iXAPXWPFy9DX2S7oeTDa",  # nosemgrep: generic.secrets.security.detected-bcrypt-hash.detected-bcrypt-hash
+        "user@example.com": "$2y$10$ZgcGQlsvMoMRmmW4Y.nUVuVHc.vOJsOA7iXAPXWPFy9DX2S7oeTDa",  # nosemgrep: generic.secrets.security.detected-bcrypt-hash.detected-bcrypt-hash
+    },
 )
 
 ui = MesopUI(auth=auth)
