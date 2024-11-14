a95b5b19191a7fde2bed585a9c064656eb07e459
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-14 22:06:40 +0530

    Set scale to 1

--- {{cookiecutter.project_slug}}/scripts/deploy_to_fly_io.sh
+++ {{cookiecutter.project_slug}}/scripts/deploy_to_fly_io.sh
@@ -13,6 +13,7 @@ fly auth login
 
 echo -e "\033[0;32mDeploying to fly.io\033[0m"
 fly deploy --config fly.toml --yes
+fly scale count 1 --config fly.toml --yes
 
 echo -e "\033[0;32mSetting secrets\033[0m"
 fly secrets set OPENAI_API_KEY=$OPENAI_API_KEY
