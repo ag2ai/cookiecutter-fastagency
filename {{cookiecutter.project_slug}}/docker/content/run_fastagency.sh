e7aeab72da9453bc5a6c9e7cd5680f5f331fbbd7
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-25 12:35:16 +0530

    Expose all the service ports in fly.io (#52)
    * WIP: Expose all the ports in docker via nginx
    
    * Expose all the service ports in fly.io
    
    * Fix end of file trailing extra newline issue

--- {{cookiecutter.project_slug}}/docker/content/run_fastagency.sh
+++ {{cookiecutter.project_slug}}/docker/content/run_fastagency.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 # Accept env variable for PORT
-{% if "nats" in cookiecutter.app_type %}
-NATS_FASTAPI_PORT=${NATS_FASTAPI_PORT:-8000}
-{% endif %}
-{% if "fastapi" in cookiecutter.app_type %}
-FASTAPI_PORT=${FASTAPI_PORT:-8008}
-{% endif %}
-{% if "mesop" in cookiecutter.app_type %}
+export NATS_FASTAPI_PORT=${NATS_FASTAPI_PORT:-8000}
+export FASTAPI_PORT=${FASTAPI_PORT:-8008}
 export MESOP_PORT=${MESOP_PORT:-8888}
-export SERVICE_PORT=$MESOP_PORT
-{% else %}
-export SERVICE_PORT=$FASTAPI_PORT
-{% endif %}
+
 # Default number of workers if not set
 WORKERS=${WORKERS:-1}
 echo "Number of workers: $WORKERS"
@@ -24,10 +16,23 @@ echo "Fly machine ID: $FLY_MACHINE_ID"
 # Generate nginx config
 for ((i=1; i<$WORKERS+1; i++))
 do
-	PORT=$((SERVICE_PORT + i))
+	PORT=$((MESOP_PORT + i))
+    sed -i "19i\    server 127.0.0.1:$PORT;" nginx.conf.template
+done
+
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((FASTAPI_PORT + i))
+    sed -i "12i\    server 127.0.0.1:$PORT;" nginx.conf.template
+done
+
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((NATS_FASTAPI_PORT + i))
     sed -i "5i\    server 127.0.0.1:$PORT;" nginx.conf.template
 done
-envsubst '${SERVICE_PORT},${FLY_MACHINE_ID}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
+
+envsubst '${NATS_FASTAPI_PORT},${FASTAPI_PORT},${MESOP_PORT},${FLY_MACHINE_ID}' < nginx.conf.template >/etc/nginx/conf.d/default.conf
 echo "Nginx config:"
 cat /etc/nginx/conf.d/default.conf
 
@@ -35,27 +40,30 @@ cat /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;" &
 {% if "nats" in cookiecutter.app_type %}
 # Run nats uvicorn server
-uvicorn {{cookiecutter.project_slug}}.deployment.main_1_nats:app --host 0.0.0.0 --port $NATS_FASTAPI_PORT > /dev/stdout 2>&1 &
+# Start multiple single-worker uvicorn instances on consecutive ports
+for ((i=1; i<$WORKERS+1; i++))
+do
+	PORT=$((NATS_FASTAPI_PORT + i))
+    echo "Starting nats fastapi uvicorn on port $PORT"
+    uvicorn {{cookiecutter.project_slug}}.deployment.main_1_nats:app --workers=1 --host 0.0.0.0 --port $PORT > /dev/stdout 2>&1 &
+done
 {% endif %}
-{% if cookiecutter.app_type == "fastapi" %}
+{% if "fastapi" in cookiecutter.app_type %}
 # Run uvicorn server
 # Start multiple single-worker uvicorn instances on consecutive ports
 for ((i=1; i<$WORKERS+1; i++))
 do
-	PORT=$((SERVICE_PORT + i))
-    echo "Starting gunicorn on port $PORT"
-    uvicorn {{cookiecutter.project_slug}}.deployment.main_1_fastapi:app --workers=1 --host 0.0.0.0 --port $PORT > /dev/stdout 2>&1 &
+	PORT=$((FASTAPI_PORT + i))
+    echo "Starting fastapi uvicorn on port $PORT"
+    uvicorn {{cookiecutter.project_slug}}.deployment.main{% if "nats" in cookiecutter.app_type %}_2_fastapi{% elif "fastapi" in cookiecutter.app_type %}_1_fastapi{% endif %}:app --workers=1 --host 0.0.0.0 --port $PORT > /dev/stdout 2>&1 &
 done
-{% else %}
-# Run uvicorn server
-uvicorn {{cookiecutter.project_slug}}.deployment.main_{% if "nats" in cookiecutter.app_type %}2_fastapi{% elif "fastapi" in cookiecutter.app_type %}1_fastapi{% endif %}:app --host 0.0.0.0 --port $FASTAPI_PORT > /dev/stdout 2>&1 &
 {% endif %}
 {% if "mesop" in cookiecutter.app_type %}
 # Run gunicorn server
 # Start multiple single-worker gunicorn instances on consecutive ports
 for ((i=1; i<$WORKERS+1; i++))
 do
-	PORT=$((SERVICE_PORT + i))
+	PORT=$((MESOP_PORT + i))
     echo "Starting gunicorn on port $PORT"
     gunicorn --workers=1 {{cookiecutter.project_slug}}.deployment.main{% if "nats" in cookiecutter.app_type %}_3_mesop{% elif "fastapi" in cookiecutter.app_type %}_2_mesop{% endif %}:app --bind 0.0.0.0:$PORT > /dev/stdout 2>&1 &
 done
