eec1307346ea90948d39fee7d1b43fcdebfc51e4
Kumaran Rajendhiran <kumaran@airt.ai>
2024-11-28 17:50:34 +0530

    Add azure as an deployment option and add azure deployment script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,6 +9,11 @@ REMOVE_PATHS = [
     {% if 'nats' not in cookiecutter.app_type %}".devcontainer/nats_server.conf",{% endif %}
     {% if cookiecutter.app_type == 'fastapi' %}"{{cookiecutter.project_slug}}/deployment/main_2_mesop.py",{% endif %}
     {% if cookiecutter.authentication != 'google' %}"deployment/",{% endif %}
+    {% if cookiecutter.deployment != 'fly.io' %}"scripts/register_to_fly_io.sh",{% endif %}
+    {% if cookiecutter.deployment != 'fly.io' %}"scripts/deploy_to_fly_io.sh",{% endif %}
+    {% if cookiecutter.deployment != 'fly.io' %}"fly.toml",{% endif %}
+    {% if cookiecutter.deployment != 'fly.io' %}".github/workflows/deploy_to_fly_io.yml",{% endif %}
+    {% if cookiecutter.deployment != 'azure' %}"scripts/deploy_to_azure.sh",{% endif %}
 ]
 
 for path in REMOVE_PATHS:
--- scripts/test_cookiecutter.sh
+++ scripts/test_cookiecutter.sh
@@ -8,20 +8,20 @@
 set -o errexit
 
 # Accept two arguments: app-type and python-version
-echo -e "\033[32mGenerating project using cookiecutter template with app-type: $1, python-version: $2 and authentication: $3\033[0m"
-rm -rf generated/$1-$2-$3
-cookiecutter -f --no-input --output-dir generated/$1-$2-$3/ ./ app_type=$1 python_version=$2 authentication=$3
+echo -e "\033[32mGenerating project using cookiecutter template with app-type: $1, python-version: $2, authentication: $3 and deployment: $4\033[0m"
+rm -rf generated/$1-$2-$3-$4
+cookiecutter -f --no-input --output-dir generated/$1-$2-$3-$4/ ./ app_type=$1 python_version=$2 authentication=$3 deployment=$4
 
 # Install generated project's dependencies
 echo -e "\033[32mInstalling dependencies for the generated project\033[0m"
-cd generated/$1-$2-$3/my_fastagency_app && pip install -e .[dev] && cd ../../..
+cd generated/$1-$2-$3-$4/my_fastagency_app && pip install -e .[dev] && cd ../../..
 
 # Initialize git in the generated project(needed for pre-commit)
 echo -e "\033[32mInitializing git in the generated project\033[0m"
-cd generated/$1-$2-$3/my_fastagency_app && git init && git add . && cd ../../..
+cd generated/$1-$2-$3-$4/my_fastagency_app && git init && git add . && cd ../../..
 # uncomment this for debugging
-# cd generated/$1-$2-$3/my_fastagency_app && git commit -m "init" --no-verify && cd ../../..
+# cd generated/$1-$2-$3-$4/my_fastagency_app && git commit -m "init" --no-verify && cd ../../..
 
 # Run pre-commit
 echo -e "\033[32mRunning pre-commit\033[0m"
-cd generated/$1-$2-$3/my_fastagency_app && pre-commit run --show-diff-on-failure --color=always --all-files && cd ../../..
+cd generated/$1-$2-$3-$4/my_fastagency_app && pre-commit run --show-diff-on-failure --color=always --all-files && cd ../../..
