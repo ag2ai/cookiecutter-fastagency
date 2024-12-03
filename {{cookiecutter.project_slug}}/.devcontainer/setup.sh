79c2b9fc3c24768c679f2a2e8bbbad9e7807ee0f
Kumaran Rajendhiran <kumaran@airt.ai>
2024-12-03 10:20:48 +0530

    Add azure as a deployment option (#56)
    * Remove no-new-privileges as sudo is needed to install azure cli
    
    * Add azure as an deployment option and add azure deployment script
    
    * Make fly.io session affinity nginx settings only for fly.io
    
    * Setup session affinity for azure
    
    * Fix json issue
    
    * Add deploy to azure CI workflow
    
    * Throw pre-commit warning only for fly.io and fix linting issues
    
    * Install azure cli for azure deployment option
    
    * Fix issue in azure acr name
    
    * WIP: Expose services' ports
    
    * Fix pre-commit issue

--- {{cookiecutter.project_slug}}/.devcontainer/setup.sh
+++ {{cookiecutter.project_slug}}/.devcontainer/setup.sh
@@ -6,12 +6,16 @@ pip install -e ".[dev]"
 
 # install pre-commit hooks
 pre-commit install
-
+{% if cookiecutter.deployment == 'fly.io' %}
 # install fly.io CLI and set fly.io CLI PATH in bashrc and zshrc
 curl -L https://fly.io/install.sh | sh
 echo 'export FLYCTL_INSTALL="/home/vscode/.fly"' | tee -a ~/.bashrc ~/.zshrc
 echo 'export PATH="$FLYCTL_INSTALL/bin:$PATH"' | tee -a ~/.bashrc ~/.zshrc
-
+{% endif %}{% if cookiecutter.deployment == 'azure' %}
+# install azure CLI
+# nosemgrep: bash.curl.security.curl-pipe-bash.curl-pipe-bash
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+{% endif %}
 # check OPENAI_API_KEY environment variable is set
 if [ -z "$OPENAI_API_KEY" ]; then
     echo
